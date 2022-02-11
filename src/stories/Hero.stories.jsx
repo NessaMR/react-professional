0feadad1893f06aa8c51c2af6b042b7518ed466e
NessaMR <vanessa.mesalira@gmail.com>
2022-02-11 07:49:32 -0300

    Creating Heading Component

--- src/stories/Hero.stories.jsx
+++ src/stories/Hero.stories.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import Hero from "../components/Hero";
-import BgGalaxy from "./assets/galaxy.jpg";
+import Heading from "../components/Heading";
+
+import Background from "./assets/nature.jpg";
 
 export default {
   title: "Hero",
@@ -9,21 +11,20 @@ export default {
 };
 
 export const usage = () => (
-  <Hero title="O essencial é invisível aos olhos." image={BgGalaxy}>
+  <Hero image={BgGalaxy}>
+    <h1> O essencial é invisível aos olhos. </h1>
     <p> A auto escola lider em aprovação </p>
   </Hero>
 );
 
 export const withList = () => (
-  <Hero
-    title={
-      <span>
+  <Hero image={Background}>
+    <Heading>
+      <h1>
         O essencial é <strong>invisível</strong>
         <br /> aos olhos.
-      </span>
-    }
-    image={BgGalaxy}
-  >
+      </h1>
+    </Heading>
     <ul>
       <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
       <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
